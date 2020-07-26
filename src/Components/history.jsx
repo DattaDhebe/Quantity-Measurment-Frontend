d7dce54d57404231e1aa0ff1612c3467a93f4d0a
DattaDhebe <dattadhebe75@gmail.com>
2020-07-26 01:01:57 +0000

    Added : History table and it css which can hold how many operations are performed.

---
+++ src/Components/history.jsx
@@ -0,0 +1,65 @@
+import React from "react";  
+import Table from '@material-ui/core/Table';  
+import TableBody from '@material-ui/core/TableBody';  
+import TableCell from '@material-ui/core/TableCell';  
+import TableContainer from '@material-ui/core/TableContainer';  
+import TableHead from '@material-ui/core/TableHead';  
+import TableRow from '@material-ui/core/TableRow';  
+import Paper from '@material-ui/core/Paper';  
+import "../CSS/table.css";
+import historyService from '../Services/unitServices'
+let service = new historyService();
+
+export class History extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      History: []
+    }
+  }
+
+ componentDidMount()
+ {
+   this.history();
+ }
+ history=() => {
+    service.history().then((data)=>{
+      console.log(" Data Extacted succefully ",data.data.data);
+      this.setState({History:data.data.data});
+      console.log("History Array",this.state.History);
+    }).catch((err)=>{
+      console.log(err);
+      
+    })
+  }
+  render() {
+    return (
+      <TableContainer component={Paper}> 
+      <h2 align="center">History </h2>
+        <Table stickyHeader aria-label="sticky table" id='History'>  
+          <TableHead>  
+            <TableRow>  
+              <TableCell align="center" >Option Type</TableCell>  
+              <TableCell align="center" >Value</TableCell>  
+              <TableCell align="center" >Result</TableCell>  
+              <TableCell align="center" >Date Of Creation</TableCell>  
+            </TableRow>  
+          </TableHead>  
+
+          <TableBody>  
+            {  
+              this.state.History.map((d, index) => {  
+                return <TableRow key={index}>  
+                  <TableCell align="center">{d.optionType}</TableCell>  
+                  <TableCell align="center">{d.value}</TableCell>  
+                  <TableCell align="center">{d.result}</TableCell>  
+                  <TableCell align="center">{d.dateOnCreation}</TableCell>   
+                </TableRow>  
+              })  
+            }  
+          </TableBody>  
+        </Table>  
+      </TableContainer>  
+    );
+  }
+}
